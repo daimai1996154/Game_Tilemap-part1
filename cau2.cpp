e116dbc5cb6a7c69ffe368cf53e3205acdbcf57d
Mai Dai <56836619+daimai1996154@users.noreply.github.com>
2019-10-23 14:17:35 +0700

    Add files via upload

---
+++ cau2.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int nhap();
+int Tong(int n);
+int nhap()
+{
+	int n;
+	do{
+		printf("Nhap vao mot so nguyen duong: \n");
+		scanf("%d",&n);
+	  }while(n<0);
+	  return n;
+}
+int Tong(int n)
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
